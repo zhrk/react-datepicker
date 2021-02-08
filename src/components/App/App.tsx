ec4a3f86e3b84d51dec517dd32461ed0473891c5
Zakhar Shchipets <zshchipets@gmail.com>
2021-02-09 00:01:49 +0300

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+import DatePicker from '../DatePicker';
+
+const App: React.FC = () => (
+  <div className={styles.container}>
+    <DatePicker />
+  </div>
+);
+
+export default App;
